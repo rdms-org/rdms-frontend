06359e084c684c38bb15a77b7fa09221509f1d67
jamie2779 <jamie2779@naver.com>
2023-09-24 23:38:17 +0900

    feat: Use proxy server

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { createStore } from 'vuex'
 const store = createStore({
   state(){
     return {
-      apiURL:"http://localhost:5000/api",
+      apiURL:"/api",
       salt:"$2a$12$Pzck2SUWG.WcYYmSno9gke",
     }
   },
